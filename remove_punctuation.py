b9528fd3641c82033e1976682d1e430898d6f22a
krasigarev <kgarev@gmail.com>
2024-07-23 10:59:01 +0300

    conditional_statements
    Signed-off-by: krasigarev <kgarev@gmail.com>

---
+++ remove_punctuation.py
@@ -0,0 +1,14 @@
+# define punctuation
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+# my_str = "Hello!!!, he said ---and went."
+# To take input from the user
+my_str = input("Enter a string: ")
+# remove punctuation from the string
+
+no_punt = ""
+
+for char in my_str:
+    if char not in punctuations:
+        no_punt = no_punt + char
+    # display the unpunctuated string
+    print(no_punt)
