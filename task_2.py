4f86282992cc5dc5cf2d0ede3822de53201af601
krasigarev <kgarev@gmail.com>
2024-07-24 09:00:40 +0300

    conditional_statements
    Signed-off-by: krasigarev <kgarev@gmail.com>

---
+++ task_2.py
@@ -0,0 +1,27 @@
+max_bad_grades = int(input())
+
+bad_grades_count = 0
+total = 0
+problems_count = 0
+last_problems = None
+
+while bad_grades_count <= max_bad_grades:
+    problem = input()
+
+    if problem == "Enough":
+        break
+    grade = int(input())
+    if grade <= 4:
+        bad_grades_count += 1
+
+    total += grade
+    problems_count += 1
+    last_problems = problem
+
+if bad_grades_count == max_bad_grades:
+    print(f"You need a break, {bad_grades_count} poor grades.")
+else:
+    average = total / problems_count
+    print(f"Average score: {average:.2f}")
+    print(f"Number of problems: {problems_count}")
+    print(f"Last problem: {last_problems}")
