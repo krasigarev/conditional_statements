704305dfd8e930739e6c44ee4df9dbd563a983dc
krasigarev <kgarev@gmail.com>
2024-07-24 15:31:53 +0300

    conditional_statements
    Signed-off-by: krasigarev <kgarev@gmail.com>

---
+++ coins.py
@@ -0,0 +1,36 @@
+import math
+
+money = float(input())
+
+coins_counter = 0
+money *= 100
+
+while money > 0:
+    if money >= 200:
+        coins_counter += 1
+        money -= 200
+    elif money >= 100:
+        coins_counter += 1
+        money -= 100
+    elif money >= 50:
+        coins_counter += 1
+        money -= 50
+    elif money >= 20:
+        coins_counter += 1
+        money -= 20
+    elif money >= 10:
+        coins_counter += 1
+        money -= 10
+    elif money >= 5:
+        coins_counter += 1
+        money -= 5
+    elif money >= 2:
+        coins_counter += 1
+        money -= 2
+    elif money >= 1:
+        coins_counter += 1
+        money -= 1
+
+    money = math.floor(money)
+
+print(coins_counter)
